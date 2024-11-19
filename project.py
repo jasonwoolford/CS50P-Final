9fe10a631c4934a362a34b777b086a0a45818a62
Jason Woolford <jason.woolford115@gmail.com>
2024-11-19 15:33:05 -0500

    Some initial main() code

--- project.py
+++ project.py
@@ -1,8 +1,13 @@
 #CS50P Final Project - Jason Woolford
 
-def main():
-    ...
+from pyfiglet import Figlet
 
+def main():
+    f = Figlet(font='slant')
+    print(f.renderText('Rock, Paper, Scissors!'))
+    name = input("Hello!  Welcome to Rock, Paper, Scissors!  We'll be keeping score, so what's your name? ")
+
+'''
 def func1():
     ...
 
@@ -11,4 +16,6 @@ def func2():
 
 def func3():
     ...
+'''
 
+main()
