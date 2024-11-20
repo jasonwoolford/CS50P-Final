7aea46440253cd607b31f978b7b4d6bbb33b6b6e
Jason Woolford <jason.woolford115@gmail.com>
2024-11-19 19:53:56 -0500

    Added game logic for determing per-round wins which will add to the scores

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 #CS50P Final Project - Jason Woolford
 
 from pyfiglet import Figlet
+import random
 
 def main():
     f = Figlet(font='slant')
@@ -20,17 +21,50 @@ def gauntlet(count):
         game()
         if game() == 1:
             user_score += 1
-        else:
+        elif game() == 2:
             comp_score += 1
+        else:
+            continue
     
     if i == count:
         finish(user_score, comp_score)
     
 
 def game():
-    usr_move = input("Select your move: Rock, Paper, or Scissors: ").lower()
+    usr_move = input("Select your move: Rock, Paper, or Scissors: ").lower().strip()
+        if usr_move == "rock":
+            usr_move == 1
+        elif usr_move == "paper":
+            usr_move == 2
+        elif usr_move == "scissors":
+            usr_move == 3
+        else:
+            raise ValueError("Try again, and please type either rock, paper, or scissors")
+            game()
 
+    cpu_move = random.randrange(1,3)
+    
 
+    if usr_move == 1 and cpu_move == 3:
+        print("Rock beats scissors!  You win the round!")
+        return 1
+    elif usr_move == 2 and cpu_move == 1:
+        print("Paper beats rock!  You win the round!")
+        return 1
+    elif usr_move == 3 and cpu_move == 2:
+        print("Scissors beats paper!  You win the round!")
+        return 1
+    elif usr_move == cpu_move:
+        print("Draw!  Neither player scores this round!")
+        return 3
+    elif cpu_move == 1 and usr_move == 3:
+        print("Rock beats scissors!  The computer wins this round!")
+        return 2 
+    elif cpu_move == 2 and usr_move == 1:
+        print("Paper beats rock!  The computer wins this round!")
+        return 2
+    elif cpu_move == 3 and usr_move == 2:
+        print("Scissors beats paper!  The computer wins this round!")
 
 '''
 def func3():
