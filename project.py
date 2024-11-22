4a88fb8ace414538290fac59cb202c1dad100c4d
Jason Woolford <jason.woolford115@gmail.com>
2024-11-22 04:29:41 -0500

    Moved around some game logic

--- project.py
+++ project.py
@@ -7,15 +7,15 @@ def main():
     f = Figlet(font='slant')
     print(f.renderText('Rock, Paper, Scissors!'))
     name = input("Hello!  Welcome to Rock, Paper, Scissors!  We'll be keeping score, so what's your name? ")
-    count = int(input(f"Good luck, {name}!  How many games would you like to play?"))
+    count = int(input(f"Good luck, {name}!  How many games would you like to play? "))
     gauntlet(count)
 
 def gauntlet(count):
+    user_score: 0
+    comp_score: 0
     count = count
     print(f"We'll now play {count} games!")
     i = 0
-    user_score: 0
-    comp_score: 0
 
     while i < count:
         while True:
@@ -32,30 +32,43 @@ def gauntlet(count):
                     raise ValueError
 
                 cpu_move = random.randrange(1,3)
+                compare(usr_move,cpu_move)
+
+                if compare() == 1:
+                    user_score += 1
+                elif compare() == 2:
+                    comp_score += 1
                 
+            except ValueError:
+                print("Try that again, and please choose rock, paper, or scissors")
+                continue
 
-                if usr_move == 1 and cpu_move == 3:
-                    print("Rock beats scissors!  You win the round!")
-                    user_score += 1
-                elif usr_move == 2 and cpu_move == 1:
-                    print("Paper beats rock!  You win the round!")
-                    user_score += 1
-                elif usr_move == 3 and cpu_move == 2:
-                    print("Scissors beats paper!  You win the round!")
-                    user_score += 1
-                elif usr_move == cpu_move:
-                    print("Draw!  Neither player scores this round!")
-                elif cpu_move == 1 and usr_move == 3:
-                    print("Rock beats scissors!  The computer wins this round!")
-                    comp_score += 1
-                elif cpu_move == 2 and usr_move == 1:
-                    print("Paper beats rock!  The computer wins this round!")
-                    comp_score += 1
-                elif cpu_move == 3 and usr_move == 2:
-                    print("Scissors beats paper!  The computer wins this round!")
-                    comp_score += 1
+def compare(usr_move, cpu_move):
+    usr_move = usr_move
+    cpu_move = cpu_move
 
-
+    if usr_move == 1 and cpu_move == 3:
+        print("Rock beats scissors!  You win the round!")
+        return 1
+    elif usr_move == 2 and cpu_move == 1:
+        print("Paper beats rock!  You win the round!")
+        return 1
+    elif usr_move == 3 and cpu_move == 2:
+        print("Scissors beats paper!  You win the round!")
+        return 1
+    elif usr_move == cpu_move:
+        print("Draw!  Neither player scores this round!")
+        return 3
+    elif cpu_move == 1 and usr_move == 3:
+        print("Rock beats scissors!  The computer wins this round!")
+        return 2
+    elif cpu_move == 2 and usr_move == 1:
+        print("Paper beats rock!  The computer wins this round!")
+        return 2
+    elif cpu_move == 3 and usr_move == 2:
+        print("Scissors beats paper!  The computer wins this round!")
+        return 2
+    
 
 def finish(name, usr_score, cpu_score):
     print("FINISH!")
