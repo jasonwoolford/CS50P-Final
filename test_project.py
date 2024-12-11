76e505dc28ba52123e374e8118acde0df12f1b00
Jason Woolford <jason.woolford115@gmail.com>
2024-12-10 21:24:53 -0500

    Added some functions to the test file

--- test_project.py
+++ test_project.py
@@ -1,1 +1,9 @@
 #This file is for running tests on project.py using pytest
+
+def test_main():
+
+def test_gauntlet(count, scores):
+
+def test_compare(usr_move,cpu_move):
+
+def test_finish(name, usr_score, cpu_score):
