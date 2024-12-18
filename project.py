f3538ac3c0aafe0ed509b60f4c07613902bf6b45
Jason Woolford <jason.woolford115@gmail.com>
2024-12-18 15:08:15 -0500

    Added spacing around lines of code and reworked some of the endgame messages.  Ready to go!

--- project.py
+++ project.py
@@ -14,9 +14,10 @@ def main():
     colored_title = (colorama.Fore.RED + f.renderText('Rock, ') + colorama.Fore.WHITE + f.renderText('Paper, ') + colorama.Fore.BLUE + f.renderText('Scissors!') + colorama.Style.RESET_ALL)
     print(colored_title)
 
-    #Get user's name for the sake of score keeping, and a color of their choosing!
+    #Get user's name for the sake of score keeping
     name = input("Hello!  Welcome to Rock, Paper, Scissors!  We'll be keeping score, so what's your name? ")
 
+    #Also get a color for highlighting the user's name and score during play
     while True:
         try:
             usr_color = input("Now choose a color for your username: Red, Green, Yellow, Blue, Magenta, Cyan, or White: ").lower().strip()
@@ -28,9 +29,11 @@ def main():
         except ValueError as color_fail:
             print(color_fail)
     
+    #Creating color values to pass along through the program
     color_name = getattr(colorama.Fore, usr_color.upper())
     cpu_color_selector = random.choice(["red","green","yellow","blue","magenta","cyan","white"])
     color_cpu = getattr(colorama.Fore, cpu_color_selector.upper())
+
     #Get number of game rounds to play from the user
     count = int(input(f"Good luck, {name}! How many games would you like to play? "))
 
@@ -40,19 +43,16 @@ def main():
     finish(name, scores[0], scores[1],f) #Once the number of rounds are finished, call upon finish() to call the winner of the game
 
 def gauntlet(count, scores, usr_color, color_name, color_cpu, name):
-    usr_color = usr_color
-    color_name = color_name
-    name = name
-    color_cpu = color_cpu
-
     usr_score = scores[0]
     cpu_score = scores[1]
+
     print(f"We'll now play {count} games!")
+    os.system('cls||clear')
 
     #Initialize for loop that handles the user-defined number of rounds of RPS
     for _ in range(count):
-        #ADD WIDTH BORDER HERE
-        print(f'{color_name}{name}:'+ colorama.Style.RESET_ALL + f' {usr_score} !VS! ' + f'{color_cpu}CPU: {cpu_score}' + colorama.Style.RESET_ALL)
+        #Prints the colorized readout of the player names and scores
+        print(f'{color_name}{name}: {usr_score} ' + colorama.Style.RESET_ALL + '!VS! ' + f'{color_cpu}CPU: {cpu_score}' + colorama.Style.RESET_ALL)
         while True: #This is for handling ValueErrors
             try:
                 #User makes a move
@@ -72,20 +72,24 @@ def gauntlet(count, scores, usr_color, color_name, color_cpu, name):
 
         #Run compare() to determine the winner for the round, store the result in the result variable
         result = compare(usr_move,cpu_move)
-        os.system('cls||clear')
+
+        os.system('cls||clear') #Clears terminal for keeping a clean interface
+
         #If the user wins, add one point to usr_score, and print a confirmation message for visual feedback
         if result == 1:
             usr_score += 1
             print(f"{usr_move.capitalize()} beats the computer's {cpu_move}, you win this round!")
+
         #If the computer wins, add on point to cpu_score, and print a confrimation message for visual feedback
         elif result == -1:
             cpu_score += 1
             print(f"{cpu_move.capitalize()} beats {usr_move}, the computer wins this round!")
+
         #If both players make the same move, confirm that the round is a tie and leave the scores unaltered
         else:
             print(f"You both played {usr_move}, this round is a tie!")
         
-    os.system('cls||clear')
+    os.system('cls||clear') #Clears terminal for keeping a clean interface
 
     scores[0] = usr_score
     scores[1] = cpu_score
@@ -104,16 +108,16 @@ def compare(usr_move, cpu_move):
         return -1
 
 def finish(name, usr_score, cpu_score,f):
-    f = f
     colored_finish = (colorama.Fore.GREEN + f.renderText('FINISH!') + colorama.Style.RESET_ALL)
     print(colored_finish)
     colored_End_Results = (colorama.Fore.GREEN + f.renderText(f"{name}: {usr_score}, CPU: {cpu_score}"))
+    
     #If the user wins...
     if usr_score > cpu_score:
-        print(f"Nice! {name} beats the computer with a score of {usr_score} over the computer's score of {cpu_score}!")
+        print(f"Nice! {name} beats the computer!")
     #If the computer wins...
     elif usr_score < cpu_score:
-        print(f"The computer wins the game with a score of {cpu_score}.  You scored {usr_score} winning rounds.  Better luck next time!")
+        print(f"The computer wins the game.  Better luck next time!")
     #If the computer and the user score equally...
     else:
         print(f"It's a tie!  Both {name} and the computer scored {usr_score} winning rounds.")
